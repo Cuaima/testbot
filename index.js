d99acb3e753dada238fce7b49469e6449422e6ae
Claudia <cl@udia.me>
2021-02-14 20:32:05 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+// require('dotenv').config()
+client.login('ODEwNTc3ODcyMTQ2MDA2MDM2.YClreA.CxyFSbV6p9mIxbsVWhQJ4q4tvY8');
