82834f44ab4296b6a0f0e9924ad8c9f94d9e530e
Claudia <cl@udia.me>
2021-02-14 21:45:34 +0100

    hid bot token, added .env documentation and made the bot reply

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ const client = new Discord.Client();
 require('dotenv').config()
 client.login(process.env.BOT_TOKEN);
 
-// console.log(process.env.BOT_TOKEN);
+client.on('ready', readyDiscord);
+// show that the bot is working on the console
+function readyDiscord() {
+    console.log('The claw test bot is ready!')
+}
 
-// client.on
+const replies = [
+    'awesome!',
+    'very cool 👍',
+    'cool cool cool',
+    'yeah!!!',
+]
+
+client.on('message', gotMessage);
+
+function gotMessage(msg){
+    if (msg.channel.id == '701186033009557584' && msg.content === 'cool') {
+        // msg.reply('cool cool cool'); //treplies to a user with a mention
+        const index = Math.floor(Math.random() * replies.length);
+        msg.channel.send(replies[index]);
+    }
+}
